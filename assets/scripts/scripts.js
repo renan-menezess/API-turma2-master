9a673fe606379630246767b756269cc09d293867
Renan Menezes <2003rcsm@gmail.com>
2023-07-12 16:43:58 -0300

    Atividadade com o Figma Iniciada

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,37 +1,89 @@
-const linkStyles = document.createElement('link');
-linkStyles.setAttribute('rel','stylesheet');
-linkStyles.setAttribute('href','./assets/styles/style.css');
+// Meus estilos
+const style = document.createElement('link');
+style.rel = 'stylesheet';
+style.href = './assets/styles/style.css';
 
-document.head.appendChild(linkStyles);
+// Fontes do Google
+const googleapis = document.createElement('link');
+googleapis.rel = 'preconnect';
+googleapis.href = 'https://fonts.googleapis.com';
 
-function getProdutos() {
-    fetch('http://localhost:3000/produtos', {
-    method: 'GET',
-    headers: {
-        'Content-type': 'application/json'
-    }
-})
+const gstatic = document.createElement('link');
+gstatic.rel = 'preconnect';
+gstatic.href = 'https://fonts.gstatic.com';
+gstatic.crossOrigin = '';
 
-    .then(resposta => resposta.json())
-    .then(resposta => {
+const fontUbuntu = document.createElement('link');
+fontUbuntu.rel = 'stylesheet';
+fontUbuntu.href = 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap';
 
-        document.querySelector('#listaProdutos').innerHTML = "";
+// Appending os links de estilos no head
+document.head.append(googleapis);
+document.head.append(gstatic);
+document.head.append(fontUbuntu);
+document.head.append(style);
 
-        for(let i = 0; i < resposta.length; i++) {
+function getProdutos(btnDelete = false) {
 
-            const ul = document.createElement("ul");
-            ul.classList.add('produto');
-
-            const img = document.createElement('img');
-            img.setAttribute('height', '50');
-
-            ul.appendChild(document.createElement('li')).innerHTML = resposta[i].id;
-            ul.appendChild(document.createElement('li')).innerHTML = resposta[i].descricao;
-            ul.appendChild(document.createElement('li')).innerHTML = resposta[i].preco;
-            ul.appendChild(document.createElement('li')).appendChild(img).setAttribute('src', resposta[i].imagem);
-
-            document.querySelector('#listaProdutos').appendChild(ul);
-
+    fetch('http://localhost:3000/produtos/', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
         }
-    });
+    })
+        .then(resposta => resposta.json())
+        .then(resposta => {
+
+            const listaProdutos = document.querySelector('#listaProdutos');
+            listaProdutos.innerHTML = "Lista de Produtos:";
+            listaProdutos.classList.add('lista-produtos');
+
+            for (let i = 0; i < resposta.length; i++) {
+
+                const ul = document.createElement('ul');
+                ul.classList.add('produto');
+                ul.id = `produto-${resposta[i].id}`;
+                ul.tabIndex = resposta[i].id;
+
+                const img = document.createElement('img');
+                img.setAttribute('src', `./assets/imagem/${resposta[i].imagem}`);
+                img.setAttribute('height', '50');
+                img.setAttribute('data-produto', 'imagem');
+
+                const liId = document.createElement('li');
+                liId.innerHTML = resposta[i].id;
+                liId.setAttribute('data-produto', 'id');
+                
+                const liDescricao = document.createElement('li');
+                liDescricao.innerHTML = resposta[i].descricao;
+                liDescricao.setAttribute('data-produto', 'descricao');
+                
+                const liPreco = document.createElement('li');
+                liPreco.innerHTML = resposta[i].preco;
+                liPreco.setAttribute('data-produto', 'preco');
+                
+                const liImg = document.createElement('li');
+                liImg.appendChild(img);
+                
+                ul.append(liImg, liId, liDescricao, liPreco);
+                
+                // Início do código do botão
+                if (btnDelete) {
+
+                    const liBotao = document.createElement('li');
+    
+                    const botao = document.createElement('button');
+                    botao.type = 'button';
+                    botao.innerHTML = '❌';
+                    botao.classList.add('botao-delete');
+                    botao.value = resposta[i].id;
+    
+                    ul.appendChild(liBotao).appendChild(botao);
+                }
+                // Fim do código do botão
+
+                listaProdutos.appendChild(ul);
+            }
+
+        });
 }
